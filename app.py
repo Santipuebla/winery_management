0ce238efc8071f2f301e9dd95e27f2e799465567
rubenledesma10 <rubenledesma89@gmail.com>
2025-05-24 19:36:39 -0300

    add view crianza and embotellamiento

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config.config import DATABASE_CONNECTION_URI
-from routes.index import index
+from routes.routes_index import index
+from routes.routes_embotellamiento import embotellamiento
+from routes.routes_crianza import crianza
 
 app = Flask(__name__)
 app.register_blueprint(index)
+app.register_blueprint(embotellamiento)
+app.register_blueprint(crianza)
 
 if __name__ == '__main__':
     print("Estoy ejecutando")
---
+++ routes/routes_embotellamiento.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+embotellamiento = Blueprint('embotellamiento', __name__)
+
+@embotellamiento.route('/embotellamiento')
+def embotellamiento_page():
+    return render_template('embotellamiento/embotellamiento.html')
+
+@embotellamiento.route('/embotellamiento/editar')
+def embotellamiento_edit_page():
+    return render_template('embotellamiento/edit_embotellamiento.html')
