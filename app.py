f084d6d66da95b5e09dda41a617593ff1c75bcf4
rubenledesma10 <rubenledesma89@gmail.com>
2025-05-24 16:22:24 -0300

    add view html

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print ("Hola mundo")
+from flask import Flask
+from config.config import DATABASE_CONNECTION_URI
+from routes.index import index
+
+app = Flask(__name__)
+app.register_blueprint(index)
+
+if __name__ == '__main__':
+    print("Estoy ejecutando")
+    app.run(debug=True)
